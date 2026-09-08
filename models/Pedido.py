351cb3427faf682d1125b38b0c202aa51c9be278
KatieChiu <114950966+KatieChiu@users.noreply.github.com>
2026-09-08 12:25:22 -0600

    Delete models directory

--- models/Pedido.py
+++
@@ -1,8 +0,0 @@
-from models.Cliente import Cliente
-
-class Pedido():
-    idPedido: int
-    fecha: str
-    cliente: Cliente #relacion uno a uno
-    
-#relacion entre cliente y pedido
--- models/Ubicacion.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-@dataclass
-class Ubicacion:
-    pais: str
-    ciudad: str
-    codigoPostal: str
-    descripcion: str
-        
